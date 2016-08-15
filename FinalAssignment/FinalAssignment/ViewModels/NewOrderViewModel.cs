0209dcc73116f43d4c866e750fe268c135abf421
James <mcclellanja9@gmail.com>
2016-08-15 11:50:41 -0600

    Edited NewOrderView

--- FinalAssignment/FinalAssignment/ViewModels/NewOrderViewModel.cs
+++ FinalAssignment/FinalAssignment/ViewModels/NewOrderViewModel.cs
@@ -1,14 +1,31 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InventoryData;
 
 namespace FinalAssignment.ViewModels
 {
     class NewOrderViewModel : Screen
     {
+        private ObservableCollection<OrderItem> _OrderItems;
 
+        public ObservableCollection<OrderItem> OrderItems
+        {
+            get { return this._OrderItems; }
+            set
+            {
+                if (this._OrderItems == value)
+                {
+                    return;
+                }
+
+                this._OrderItems = value;
+                this.NotifyOfPropertyChange(() => this._OrderItems);
+            }
+        }
     }
 }
