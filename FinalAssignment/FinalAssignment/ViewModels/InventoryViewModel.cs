c0207b53c7b0ba7a4595a64efa2c0cda4f836633
Ashley Richardson <ashleyrichardson@mail.weber.edu>
2016-08-15 12:30:36 -1200

    Here you go.

--- FinalAssignment/FinalAssignment/ViewModels/InventoryViewModel.cs
+++ FinalAssignment/FinalAssignment/ViewModels/InventoryViewModel.cs
@@ -4,10 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace FinalAssignment.ViewModels
 {
     class InventoryViewModel : Screen
     {
+        private SqlConnection conn;
+        private SqlCommand cmd;
+        private SqlDataAdapter dataAdapt;
+        private DataTable dataTab;
+
+        public void ConnectDb()
+        {
+            conn = new SqlConnection();
+        }
     }
 }
